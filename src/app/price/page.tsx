b4e0db8f755784c5e689cfe8e38a56437d213ef7
nikita grebnev <n.a.grebnev@gmail.com>
2024-10-20 20:57:54 +0500

    прайс

---
+++ src/app/price/page.tsx
@@ -0,0 +1,9 @@
+export default function PricePage() {
+    return (
+        <div className="container flex flex-col mx-auto gap-4 px-4 py-16">
+            <p className="text-3xl font-semibold">Прайс-лист</p>
+            <p className="text-lg italic">Здесь вы можете увидеть стоимость моей работы</p>
+            <p className="text-xs">В час 1500 р</p>
+        </div>
+    )
+}
